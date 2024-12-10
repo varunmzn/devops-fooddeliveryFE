0e374def72ff3c6f517ecf1386b8b595d5e7c847
Varun Bhardwaj <varunmzn1987@gmail.com>
2024-12-10 17:50:33 +0530

    changes

---
+++ src/app/constants/url.ts
@@ -0,0 +1,9 @@
+// export const API_URL_RL ='http://192.168.49.2:31985';
+export const API_URL_RL ='http://hello-test.internal';
+export const API_URL_Order ='http://localhost:9094';
+export const API_URL_FC ='http://hello-test.internal';
+// export const API_URL_FC ='http://host.docker.internal:9092';
+export const API_URL_UD ='http://localhost:9093';
+
+export const K8ExternalIp = 'http://k8s-default-awsingre-3d4b7f90d4-122545836.eu-west-3.elb.amazonaws.com';
+
